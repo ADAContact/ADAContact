cdab517a098579b04e80ea3056ed008926dc4b81
paddy blake <paddyblake09@hotmail.com>
2022-01-27 13:54:16 +0000

    v2.0

---
+++ src/views/Index.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+// core components
+import IndexNavbar from "components/Navbars/IndexNavbar.js";
+import PageHeader from "components/PageHeader/PageHeader.js";
+import Footer from "components/Footer/Footer.js";
+
+// sections for this page/view
+import Purchase from "views/IndexSections/Purchase.js";
+
+export default function Index() {
+  React.useEffect(() => {
+    document.body.classList.toggle("index-page");
+    // Specify how to clean up after this effect:
+    return function cleanup() {
+      document.body.classList.toggle("index-page");
+    };
+  },[]);
+  return (
+    <>
+      <IndexNavbar />
+      <div className="wrapper">
+        <PageHeader />
+        <div className="main">
+
+          <Purchase />
+        </div>
+        <Footer />
+      </div>
+    </>
+  );
+}
